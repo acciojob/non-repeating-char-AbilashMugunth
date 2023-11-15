37773639c8e36a497ebce4143d608fc643cd62e7
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-11-15 21:45:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-function firstNonRepeatedChar(str) {
- // Write your code here
-}
+let checked = new Set([]);
+
+const firstNonRepeatedChar = function (str) {
+  for (let i = 0; i < str.length; i++) {
+    let curr = str[i];
+    if (checked.has(curr)) {
+      continue;
+    }
+    for (let j = i + 1; j < str.length; j++) {
+      if (curr == str[j]) {
+        checked.add(curr);
+        break;
+      } else if (curr !== str[j] && j == str.length - 1) {
+        // console.log(curr);
+        return curr;
+      }
+    }
+  }
+};
+
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
